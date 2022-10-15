f060d764e77d367d2489e970cb0a272bc4987c51
Gabriel Cardoso <114122280+gabrielpoukas@users.noreply.github.com>
2022-10-14 21:11:00 -0300

    atualização aprendizado Javascript

---
+++ aula005/ex008.js
@@ -0,0 +1,12 @@
+var vel = 20.1
+console.log (`a velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 60) {
+
+    console.log (' voce ultrapassou a velocidade permitida. MULTADO!')
+
+
+}
+
+
+console.log ('Dirija sempre usando cinto de segurança!')
---
+++ aula005/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'frança'
+console.log (`Vivendo em ${pais} `)
+if (pais  == 'brasil') {
+
+console.log (' Voce é um Brasileiro')
+} else {
+
+console.log (' Voce é um Estrangeiro')
+
+}
