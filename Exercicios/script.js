042a6e09e65abeef907c30309e1ecd7d8a94dfa5
Gabriel Cardoso <114122280+gabrielpoukas@users.noreply.github.com>
2022-10-18 14:45:28 -0300

    atualização

--- Exercicios/script.js
+++ Exercicios/script.js
@@ -6,7 +6,8 @@ var msg = window.document.getElementById ('msg')
 var img = window.document.getElementById ('imagem')
 var data = new Date()
 var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} Horas`
+var min = data.getMinutes()
+msg.innerHTML = `  <strong> Agora são </strong> ${hora}:${min} Horas`
 if (hora >= 0 && hora <12 ) {
 
     //Bom dia
