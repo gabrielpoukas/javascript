d4272770d9ee1e1a296b4f638dd7a3277df229ac
Gabriel Cardoso <114122280+gabrielpoukas@users.noreply.github.com>
2022-10-20 18:20:39 -0300

    projeto novo

---
+++ exercicio3/script.js
@@ -0,0 +1,54 @@
+function contar() {
+
+var ini = document.getElementById ('txt1')
+var fim = document.getElementById ('txtf')
+var passo = document.getElementById ('txtp')
+let res = document.getElementById ('res')
+
+if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+
+window.alert (' [ERRO] Faltam alguns dados!')
+
+} else {
+
+    res.innerHTML = 'Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+        
+
+
+
+
+
+        
+
+
+
+
+
+
